bb1f5381090f6d58461ede64c524f972f3477891
xiong03 <xiong3731@gmail.com>
2023-10-08 23:42:33 +0800

    2023年10月8日23点41分 添加了python学习基础时的示例代码 明天再学

---
+++ python基础/基本数据类型.py
@@ -0,0 +1,43 @@
+# 整数
+integer_var = 42
+
+# 浮点数
+float_var = 3.14159
+
+# 字符串
+string_var = "Hello, World!"
+
+# 布尔值
+bool_var = True
+
+# 列表
+list_var = [1, 2, 3, 4, 5]
+
+# 元组
+tuple_var = (10, 20, 30)
+
+# 集合
+set_var = {1, 2, 3, 4, 5}
+
+# 字典
+dict_var = {'name': 'Alice', 'age': 30}
+
+# 空值
+none_var = None
+
+# 输出各个变量的类型和值
+print(f"整数: {integer_var}, 类型: {type(integer_var)}")
+print(f"浮点数: {float_var}, 类型: {type(float_var)}")
+print(f"字符串: {string_var}, 类型: {type(string_var)}")
+print(f"布尔值: {bool_var}, 类型: {type(bool_var)}")
+print(f"列表: {list_var}, 类型: {type(list_var)}")
+print(f"元组: {tuple_var}, 类型: {type(tuple_var)}")
+print(f"集合: {set_var}, 类型: {type(set_var)}")
+print(f"字典: {dict_var}, 类型: {type(dict_var)}")
+print(f"空值: {none_var}, 类型: {type(none_var)}")
+print("====================================")
+# 强制转换
+aaaa = "10"
+bbbb = int(aaaa)
+print(type(aaaa))
+print(type(bbbb))
